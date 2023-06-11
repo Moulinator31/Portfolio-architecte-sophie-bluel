36f77550b52e60a377d9a2e3be9d0d8944779735
Elsa Tanguy <elsa.tang28@gmail.com>
2023-06-11 12:55:30 +0200

    Ajouts des images par catégories

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,22 +1,20 @@
+const gallery = document.querySelector('.gallery');
+let filtres = document.querySelector('.filtres');
+
 async function getWorks() {
+    
     try {
         const reponse = await fetch('http://localhost:5678/api/works');
         const data = await reponse.json();
+        
 
         return data;
     }
      catch(error){
         console.error(error);
      }
-};
-
-
-const gallery = document.querySelector('.gallery');
-let filtres = document.querySelector('.filtres');
-
-
-
-let number = 0;
+    
+}
 
 async function showWorks(){
     const works = await getWorks();
@@ -30,99 +28,115 @@ async function showWorks(){
         image.setAttribute('alt', work.title);
         image.setAttribute('figcaption', work.title);
         figcaption.innerText = work.title;
+        image.setAttribute("data-category-id",work.categoryId);
+        
 
         gallery.appendChild(figure);
         figure.appendChild(image);
         figure.appendChild(figcaption);
+
+        
     });
-};
-showWorks();
+    filtre();
+}
+
 
 /** Ajout des filtres */
 
-async function getCategory() {
+async function getCategories() {
     try {
         const reponse = await fetch('http://localhost:5678/api/categories');
         const data = await reponse.json();
+
 
         return data;
     }
      catch(error){
         console.error(error);
-     }
-};
-
+    }
+}
 
 async function filtre () {
-    const category = await getCategory();
+    const categories = await getCategories();
+    
+    const buttonAll = document.createElement("button");
 
-    category.forEach(category => {
+    buttonAll.setAttribute("data-filtre-id", "all");
+    buttonAll.setAttribute("class","filtresborder");
+    buttonAll.innerText = "Tous";
+
+    filtres.appendChild(buttonAll);
+
+    categories.forEach(category => {
 
 
-    const button = document.createElement('button');
-    button.classList.add('.filterborder');
+        const button = document.createElement('button');
+        button.classList.add('.filterborder');
 
     
-
-    button.setAttribute("name",category.name);
-    button.setAttribute("class","filtresborder");
-    button.setAttribute("id", category.id);
-    button.innerText = category.name;
+    
+        button.setAttribute("name",category.name);
+        button.setAttribute("class","filtresborder");
+        button.setAttribute("data-filtre-id", category.id);
+        button.innerText = category.name;
+    
    
  
-    filtres.appendChild(button);
-   
-    
-    
-console.log(category);
-    
-});
+        filtres.appendChild(button);
+    });
+    tabsFilter();
 }
-filtre();
 
-/*Bouton du "Tous" */
+/* Mise en place des filtres par images*/
 
-const ButtonTous = document.createElement("button");
-ButtonTous.classList.add("filter");
-ButtonTous.setAttribute("id", "all");
-ButtonTous.setAttribute("class","filtresborder");
-ButtonTous.innerText = "Tous";
-filtres.appendChild(ButtonTous);
+function tabsFilter(){
+    const filterButtons = document.querySelectorAll('.filtresborder');/*Les filtres*/
+    const projets = document.querySelectorAll(".gallery img");/* Les images*/
+    console.log(projets);
+    
 
+    const resetActive = () =>{
+        filterButtons.forEach(filterButton =>{
+            filterButton.classList.remove('active');
+        });
+    };
 
-/** Changement d'images au click */
-const button = document.createElement('button');
+   /*Appel toutes les images*/
+    const Showprojets = (element) => {
+        projets.forEach(projet => {
+            let category = projet.getAttribute('data-category-id');
+            /* Button Tous*/
+            if (element === 'all'){
+                projet.parentNode.classList.remove('hide');
+                return; /*Arrête l'action ici*/
+            }
+            /* Si le filtre est différent à la categorie alors il ajoute la Class, sinon il l'enlève*/
+            if (category !== element){
+                projet.parentNode.classList.add('hide');
+            } else {
+                projet.parentNode.classList.remove('hide');
+            }
+            
+        });
 
-ButtonTous.addEventListener ('click', function() {
-
-  
-});
-
-button.addEventListener ('click', function() {
-    if (categoryId == 1){
-        categoryId = id
-    }else {
-        categoryId == all
     }
+    filterButtons.forEach(filterButton => {
+        filterButton.addEventListener("click", () => {
+            //event.preventDefault();
+            let filtre = filterButton.dataset.filtreId;
+            Showprojets(filtre);
+            resetActive();
+            filterButton.classList.add('active');
+        });
+    });
+    
+}
 
-   showWorks();
+ function init(){
+    showWorks();
+}
 
-});
-
-button.addEventListener ('click', function() {
-    category = 3;
-   showWorks();
-   filtre()
-});
-
-button.addEventListener ('click', function() {
-    category = 0;
-    showWorks()
-    filtre
-});
-
-
-
+init();
 
 
 
